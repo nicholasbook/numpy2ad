58130cafee9b6e1bc8eebdfa8796083aace25e4b
Nicholas Book <nbook@gremse-it.com>
2023-06-21 23:22:15 +0200

    removed numba benchmarks

--- benchmarks/benchmark_gls.py
+++ benchmarks/benchmark_gls.py
@@ -1,52 +1,11 @@
 import numpy as np
 from numpy2ad import transform
 import timeit
-from numba import njit
 
 
 def GLS(X, M, y):
     M_inv = np.linalg.inv(M)
     return np.linalg.inv(X.T @ M_inv @ X) @ X.T @ M_inv @ y
-
-
-@njit
-def GLS_jit(X, M, y):
-    M_inv = np.linalg.inv(M)
-    return np.linalg.inv(X.T @ M_inv @ X) @ X.T @ M_inv @ y
-
-
-@njit
-def GLS_ad_jit(X, M, y, X_a, M_a, y_a, out_a):
-    M_inv = np.linalg.inv(M)
-    v0 = X.T
-    v1 = v0 @ M_inv
-    v2 = v1 @ X
-    v3 = np.linalg.inv(v2)
-    v4 = X.T
-    v5 = v3 @ v4
-    v6 = v5 @ M_inv
-    out = v6 @ y
-    M_inv_a = np.zeros_like(M_inv)
-    v0_a = X_a.T
-    v1_a = np.zeros_like(v1)
-    v2_a = np.zeros_like(v2)
-    v3_a = np.zeros_like(v3)
-    v4_a = X_a.T
-    v5_a = np.zeros_like(v5)
-    v6_a = np.zeros_like(v6)
-    y_a += v6.T @ out_a
-    v6_a += out_a @ y.T
-    M_inv_a += v5.T @ v6_a
-    v5_a += v6_a @ M_inv.T
-    v4_a += v3.T @ v5_a
-    v3_a += v5_a @ v4.T
-    v2_a -= v3.T @ (v3_a @ v3.T)
-    X_a += v1.T @ v2_a
-    v1_a += v2_a @ X.T
-    M_inv_a += v0.T @ v1_a
-    v0_a += v1_a @ M_inv.T
-    M_a -= M_inv.T @ (M_inv_a @ M_inv.T)
-    return (out, X_a, M_a, y_a)
 
 
 def benchmark_gls(rows, func):
@@ -71,28 +30,12 @@ def benchmark_gls_ad(rows, func_ad):
     _, _, _, _ = func_ad(X, M, y, X_a, M_a, y_a, out_a)
 
 
-def warm_up_the_jit():
-    rows = 64
-    X = np.random.rand(rows, rows)
-    M = np.random.rand(rows, rows)
-    y = np.random.rand(rows, 1)
-    X_a = np.zeros_like(X)
-    M_a = np.zeros_like(M)
-    y_a = np.zeros_like(y)
-    out_a = np.zeros_like(y)
-
-    _ = GLS_jit(X, M, y)
-    _, _, _, _ = GLS_ad_jit(X, M, y, X_a, M_a, y_a, out_a)
-
-
 if __name__ == "__main__":
     num_rows = [32, 64, 128, 256, 512, 1024, 2048, 4096]  # ~ 6 min for 4096
     export_array = np.zeros(shape=(len(num_rows), 5))
 
     # generate GLS_ad
     exec(compile(transform(GLS), filename="<ast>", mode="exec"))
-
-    warm_up_the_jit()
 
     average_over = 10
     for i, row in enumerate(num_rows):
@@ -104,17 +47,6 @@ if __name__ == "__main__":
         )
         print(f"Forward pass with {row} rows took {gls_result} seconds.")
 
-        # with Numba
-        gls_jit_result = timeit.timeit(
-            "benchmark_gls(row, GLS_jit)",
-            setup="from __main__ import benchmark_gls, GLS_jit",
-            globals=locals(),
-            number=average_over,
-        )
-        print(
-            f"[Numba JIT] Forward pass with {row} rows took {gls_jit_result} seconds."
-        )
-
         # transformed code
         gls_ad_result = timeit.timeit(
             "benchmark_gls_ad(row, GLS_ad)",
@@ -124,23 +56,11 @@ if __name__ == "__main__":
         )
         print(f"Forward and reverse pass with {row} rows took {gls_ad_result} seconds.")
 
-        gls_ad_jit_result = timeit.timeit(
-            "benchmark_gls_ad(row, GLS_ad_jit)",
-            setup="from __main__ import benchmark_gls_ad, GLS_ad_jit",
-            globals=locals(),
-            number=average_over,
-        )
-        print(
-            f"[Numba JIT] Forward and reverse pass with {row} rows took {gls_ad_jit_result} seconds.\n"
-        )
         export_array[i, :] = [
             row,
             gls_result / average_over,
-            gls_jit_result / average_over,
-            gls_ad_result / average_over,
-            gls_ad_jit_result / average_over,
+            gls_ad_result / average_over
         ]
 
     np.savetxt("timeit_gls.txt", export_array)
 
-    # TODO: plots
--- benchmarks/benchmark_mma.py
+++ benchmarks/benchmark_mma.py
@@ -1,29 +1,10 @@
 import numpy as np
 from numpy2ad import transform
 import timeit
-from numba import njit
 
 
 def MMA(A, B, C):
     return A @ B + C
-
-
-@njit
-def MMA_jit(A, B, C):
-    tmp = A @ B
-    return tmp + C
-
-
-@njit
-def MMA_ad_jit(A, B, C, A_a, B_a, C_a, out_a):
-    v0 = A @ B
-    out = v0 + C
-    v0_a = np.zeros_like(v0)
-    C_a += out_a
-    v0_a += out_a
-    B_a += A.T @ v0_a
-    A_a += v0_a @ B.T
-    return (out, A_a, B_a, C_a)
 
 
 def benchmark_mma(rows, func):
@@ -48,28 +29,12 @@ def benchmark_mma_ad(rows, func_ad):
     _, _, _, _ = func_ad(A, B, C, A_a, B_a, C_a, out_a)
 
 
-def warm_up_the_jit():
-    rows = 64
-    A = np.random.rand(rows, rows)
-    B = np.random.rand(rows, rows)
-    C = np.random.rand(rows, rows)
-    A_a = np.zeros_like(A)
-    B_a = np.zeros_like(B)
-    C_a = np.zeros_like(C)
-    out_a = np.zeros_like(C)
-
-    _ = MMA_jit(A, B, C)
-    _, _, _, _ = MMA_ad_jit(A, B, C, A_a, B_a, C_a, out_a)
-
-
 if __name__ == "__main__":
     num_rows = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]
     export_array = np.zeros(shape=(len(num_rows), 5))
 
     # generate MMA_ad
     exec(compile(transform(MMA), filename="<ast>", mode="exec"))
-
-    warm_up_the_jit()
 
     average_over = 10
     for i, row in enumerate(num_rows):
@@ -81,17 +46,6 @@ if __name__ == "__main__":
         )
         print(f"Forward pass with {row} rows took {mma_result} seconds.")
 
-        # with Numba
-        mma_jit_result = timeit.timeit(
-            "benchmark_mma(row, MMA_jit)",
-            setup="from __main__ import benchmark_mma, MMA_jit",
-            globals=locals(),
-            number=average_over,
-        )
-        print(
-            f"[Numba JIT] Forward pass with {row} rows took {mma_jit_result} seconds."
-        )
-
         # transformed code
         mma_ad_result = timeit.timeit(
             "benchmark_mma_ad(row, MMA_ad)",
@@ -100,24 +54,11 @@ if __name__ == "__main__":
             number=average_over,
         )
         print(f"Forward and reverse pass with {row} rows took {mma_ad_result} seconds.")
-
-        mma_ad_jit_result = timeit.timeit(
-            "benchmark_mma_ad(row, MMA_ad_jit)",
-            setup="from __main__ import benchmark_mma_ad, MMA_ad_jit",
-            globals=locals(),
-            number=average_over,
-        )
-        print(
-            f"[Numba JIT] Forward and reverse pass with {row} rows took {mma_ad_jit_result} seconds.\n"
-        )
+        
         export_array[i, :] = [
             row,
             mma_result / average_over,
-            mma_jit_result / average_over,
-            mma_ad_result / average_over,
-            mma_ad_jit_result / average_over,
+            mma_ad_result / average_over
         ]
 
     np.savetxt("timeit_mma.txt", export_array)
-
-    # TODO: plots
